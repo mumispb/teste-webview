9a94343a912ed61aa3ff2b65544a8dffe5e0be2c
Murilo <murilo.pereira.botelho@gmail.com>
2020-06-30 13:37:48 -0300

    Initial project commit

--- src/router.jsx
+++ src/router.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from './App';
 
 export default function Routes() {
   return (
     <Router>
       <Switch>
-        <Route path='/about'>
-          <About />
-        </Route>
         <Route path='/cadastro/:id'>
           <App />
         </Route>
@@ -18,16 +15,4 @@ export default function Routes() {
       </Switch>
     </Router>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
