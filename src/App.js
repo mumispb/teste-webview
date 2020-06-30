c0d15284363a74d5f472dc1fbf21f927f0255cb8
Daniel Beigelman <danielbeigelman@Macs-MacBook-Pro.local>
2020-06-30 17:49:43 -0300

    Ajustes na web view com parametros variaveis

--- src/App.js
+++ src/App.js
@@ -1,69 +1,62 @@
 /* global DirectCheckout */
-import React, { useRef, useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { useParams, useHistory, Link } from 'react-router-dom';
+import React, { useRef, useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useParams, useHistory, Link } from "react-router-dom";
+
+const tokenAPI =
+  "E6FB2B4BAE36A71FD404DF75AA3619DEB476159C78FB69EE8F27D3068C17D529";
 
 function App({ router }) {
   const { name, number, cvc, year, month } = useParams();
-  const history = useHistory();
   const aRef = useRef(null);
   const [hash, setHash] = useState(null);
   console.log(name, number, cvc, month, year);
 
   useEffect(() => {
     if (hash) {
-      alert('entrou no useeffect com hash');
       aRef.current.click();
     }
   }, [hash]);
 
-  function teste() {
-    const checkout = new DirectCheckout('96299D8734D8F4BAF2695F2374D383FB068088B788DE0F7FAB0E1027B526F063', false);
-    /* Em sandbox utilizar o construtor new DirectCheckout('PUBLIC_TOKEN', false); */
-    const cardData = {
-      cardNumber: '5226884528524790',
-      holderName: 'Murilo',
-      securityCode: '067',
-      expirationMonth: '01',
-      expirationYear: `2027`,
-    };
-    checkout.getCardHash(
-      cardData,
-      function (cardHash) {
-        console.log(cardHash);
-        /* Sucesso - A variável cardHash conterá o hash do cartão de crédito */
-        alert(cardHash);
-        /* history.push('/teste/?message=success'); */
-        console.log(aRef.current);
-        setHash(cardHash);
-        alert('foi');
-      },
-      function (error) {
-        /* Erro - A variável error conterá o erro ocorrido ao obter o hash */
-        alert(error.message);
-        console.log(error);
-      }
-    );
-  }
+  useEffect(() => {
+    function GenerateHash() {
+      const checkout = new DirectCheckout(tokenAPI, false);
+      /* Em sandbox utilizar o construtor new DirectCheckout('PUBLIC_TOKEN', false); */
+      const cardData = {
+        cardNumber: number,
+        holderName: name,
+        securityCode: cvc,
+        expirationMonth: month,
+        expirationYear: year,
+      };
+      checkout.getCardHash(
+        cardData,
+        function (cardHash) {
+          console.log(cardHash);
+          /* Sucesso - A variável cardHash conterá o hash do cartão de crédito */
+          /* history.push('/teste/?message=success'); */
+          setHash(cardHash);
+        },
+        function (error) {
+          /* Erro - A variável error conterá o erro ocorrido ao obter o hash */
+          console.log(error);
+        }
+      );
+    }
+    GenerateHash();
+  }, [cvc, month, name, number, year]);
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-        <button onClick={teste}>teste</button>
-        <a href='/teste/?message=success' ref={aRef}>
-          TEESSSTE
-        </a>
-        <Link to='/teste'>Ir para teste</Link>
-      </header>
-    </div>
+    <>
+      <a
+        style={{ display: "none" }}
+        href={`/teste/?message=success?hash=${hash}`}
+        ref={aRef}
+      >
+        TEESSSTE
+      </a>
+    </>
   );
 }
 
